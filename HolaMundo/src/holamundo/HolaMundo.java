0a3ceba5d1342875b859f5a088ccc973a0ad8e60
OscarSumAL <1512350@senati.pe>
2024-04-23 13:20:17 -0500

    first commit

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Oscar Alberto Apaza Limache");
+        System.out.println("¡Hola Mundo!");
+        
+    }
+    
+}
+
+
+
+
